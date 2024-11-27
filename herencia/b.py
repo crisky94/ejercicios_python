53df3f08fbc9fd6dc96e4250fceafe70c8101435
Cris <criistiiniilla.1994@gmail.com>
2024-11-27 21:46:01 +0100

    ejercicios herencia a,b,c

--- herencia/b.py
+++ herencia/b.py
@@ -1,1 +1,40 @@
 # b. Gestión de una biblioteca: Crea una clase base Material con atributos comunes (título, autor) y métodos como informacion. Luego, define subclases Libro, Revista y DVD.
+
+class Material:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor =  autor
+
+    def informacion(self):
+        return f'Título: {self.titulo} - Autor: {self.autor}'
+    
+class Libro(Material):
+    def __init__(self, titulo, autor):
+        super().__init__(titulo, autor)
+
+    def informacion(self):
+        return f'-Libro: {self.titulo} - {self.autor}'
+
+class Revista(Material):
+    def __init__(self, titulo, autor):
+        super().__init__(titulo, autor)
+
+    def informacion(self):
+        return f'-Libro: {self.titulo} - {self.autor}'
+
+class DVD(Material):
+    def __init__(self, titulo, autor):
+        super().__init__(titulo, autor)
+
+    def informacion(self):
+        return f'-Libro: {self.titulo} - {self.autor}'
+
+libro = Libro('El Quijote', 'Cervantes')
+
+revista = Revista('Revista El Jueves', 'RBA Revistas')
+
+dvd = DVD('Edward Scissorhands', 'Tim Burton')
+
+print(libro.informacion())
+print(revista.informacion())
+print(dvd.informacion())
